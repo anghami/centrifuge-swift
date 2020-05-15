1c2101d447fa6fc05d1950e41092fd3279fa39b9
Raja Baz <raja.e.baz@gmail.com>
2020-05-15 16:55:45 +0300

    Hopefully fix carthage

---
+++ SwiftCentrifuge/SwiftCentrifuge.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCentrifuge.h
+//  SwiftCentrifuge
+//
+//  Created by Raja Baz on 5/15/20.
+//  Copyright © 2020 Centrifugal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftCentrifuge.
+FOUNDATION_EXPORT double SwiftCentrifugeVersionNumber;
+
+//! Project version string for SwiftCentrifuge.
+FOUNDATION_EXPORT const unsigned char SwiftCentrifugeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCentrifuge/PublicHeader.h>
+
+
